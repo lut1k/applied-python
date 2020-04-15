79dcbcf86b5672f27cc57618d7b0048dde5a2155
lut1k <lut1k@tut.by>
2020-04-15 19:21:56 +0300

    2.Started the second HomeWork

--- homeworks/log_parse/log_parse.py
+++ homeworks/log_parse/log_parse.py
@@ -1,4 +1,6 @@
 # -*- encoding: utf-8 -*-
+import re
+
 
 def parse(
     ignore_files=False,
@@ -9,7 +11,27 @@ def parse(
     ignore_www=False,
     slow_queries=False
 ):
-    '''
-        ТУТ ДОЛЖЕН БЫТЬ ВАШ КОД
-    '''
+    probe_string = '[18/Mar/2018 11:19:40] "GET https://www.sys.mail.ru/calendar/config/254/40263/ HTTP/1.1" 200 965'
+    regexp_pattern = (r'^\[(?P<day>0[1-9]|[12][0-9]|3[01])/'
+                      r'(?P<month>Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec)/'
+                      r'(?P<year>(19|20)\d\d)\s{1}'
+                      r'(?P<hours>0[0-9]|1[0-9]|2[0-3]):'
+                      r'(?P<minutes>[0-5][0-9]):'
+                      r'(?P<seconds>[0-5][0-9])\]\s{1}"'
+                      r'')
+    match = re.search(regexp_pattern, probe_string)
+    print(match['day'],
+          match['month'],
+          match['year'],
+          match['hours'],
+          match['minutes'],
+          match['seconds'],
+          )
+
+    with open("log.log", "r", encoding='utf8') as file:
+        pass
     return []
+
+
+if __name__ == '__main__':
+    parse()
