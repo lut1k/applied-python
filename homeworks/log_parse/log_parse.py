82bc7b6bdbb01d5c0331da0d2032fd10eadcf383
lut1k <lut1k@tut.by>
2020-04-16 14:19:03 +0300

    2.Implemented top-5 urls without params

--- homeworks/log_parse/log_parse.py
+++ homeworks/log_parse/log_parse.py
@@ -1,37 +1,64 @@
 # -*- encoding: utf-8 -*-
 import re
+from collections import Counter
+
+
+FILE_WITH_LOGS = 'log.log'
 
 
 def parse(
-    ignore_files=False,
-    ignore_urls=[],
-    start_at=None,
-    stop_at=None,
-    request_type=None,
-    ignore_www=False,
-    slow_queries=False
+        ignore_files=False,
+        ignore_urls=[],
+        start_at=None,
+        stop_at=None,
+        request_type=None,
+        ignore_www=False,
+        slow_queries=False
 ):
-    probe_string = '[18/Mar/2018 11:19:40] "GET https://www.sys.mail.ru/calendar/config/254/40263/ HTTP/1.1" 200 965'
+
     regexp_pattern = (r'^\[(?P<day>0[1-9]|[12][0-9]|3[01])/'
                       r'(?P<month>Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec)/'
                       r'(?P<year>(19|20)\d\d)\s{1}'
                       r'(?P<hours>0[0-9]|1[0-9]|2[0-3]):'
                       r'(?P<minutes>[0-5][0-9]):'
                       r'(?P<seconds>[0-5][0-9])\]\s{1}"'
-                      r'')
-    match = re.search(regexp_pattern, probe_string)
-    print(match['day'],
-          match['month'],
-          match['year'],
-          match['hours'],
-          match['minutes'],
-          match['seconds'],
-          )
+                      r'(?P<method>[A-Z]+)\s{1}'
+                      r'(?P<scheme>[a-z]+)://'
+                      r'(?P<host>[a-z.]*)'
+                      r'(?P<url_path>[\w/.]+)'
+                      r'(?P<url_params>[^\s^#]+)?'
+                      r'(?P<url_anchor>[^\s]+)?\s{1}'
+                      r'(?P<protocol>[\w/.]+)"\s'
+                      r'(?P<response_code>\d{3})\s'
+                      r'(?P<response_time>\d+)'
+                      )
+    return returned_top_5_urls(logs, regexp_pattern)
 
-    with open("log.log", "r", encoding='utf8') as file:
-        pass
-    return []
+
+def created_filtered_list_with_logs(list_with_logs: list):
+    pass
+
+
+def returned_top_5_urls(filtered_list_with_logs: list, reg_pattern):
+    log_counter = {}
+    for log in filtered_list_with_logs:
+        match = re.search(reg_pattern, log)
+        if match:
+            full_url = "{host}{path}{params}{anchor}".format(host=match['host'],
+                                                             path=match['url_path'],
+                                                             params=match['url_params'] if match['url_params'] else "",
+                                                             anchor=match['url_anchor'] if match['url_anchor'] else "",
+                                                             )
+            if log_counter.get(full_url):
+                log_counter[full_url] += 1
+            else:
+                log_counter[full_url] = 1
+    sorted_log_counter = {url: log_counter[url] for url in sorted(log_counter, key=log_counter.get, reverse=True)}
+    return [count for count in sorted_log_counter.values()][:5]
 
 
 if __name__ == '__main__':
-    parse()
+    with open(FILE_WITH_LOGS, "r", encoding='utf8') as file:
+        logs = [log.rstrip() for log in file.readlines()]
+
+    print(parse(ignore_files=True))
