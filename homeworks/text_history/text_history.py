447d6437c52c78050705e296c70b8bd28f646bfd
lut1k <lut1k@tut.by>
2020-04-30 14:55:44 +0300

    Homework #3 started

--- homeworks/text_history/text_history.py
+++ homeworks/text_history/text_history.py
@@ -1,5 +1,37 @@
 class TextHistory:
-    pass
+    def __init__(self, text='', version=0):
+        self._text = text
+        self._version = version
+
+    @property
+    def text(self):
+        return self._text
+
+    @property
+    def version(self):
+        return self._version
+
+    def _check_is_position_in_length(self, position):
+        if len(self._text) < position or position < 0:
+            raise ValueError("Insert position out of string length")
+
+    def insert(self, text, pos=None):
+        if pos is None:
+            self._text += text
+        else:
+            self._check_is_position_in_length(pos)
+            self._text = self.text[:pos] + text + self.text[pos:]
+        self._version += 1
+        return self._version
+
+    def replace(self, text, pos=None):
+        if pos is None:
+            self._text += text
+        else:
+            self._check_is_position_in_length(pos)
+
+    def delete(self, pos, length):
+        pass
 
 
 class Action:
@@ -16,3 +48,13 @@ class ReplaceAction(Action):
 
 class DeleteAction(Action):
     pass
+
+
+h = TextHistory()
+print(h.text)
+print(h.version)
+h.insert('abc', 0)
+print(h.text)
+print(h.version)
+
+
